d34fe6ef0e3d937451b6a8091951e763c1794846
jun9ho <wjdgh9806@korea.ac.kr>
2026-07-02 21:38:51 +0900

    fix: Experience Ctrl RequestBody import 수정

--- backend/coverletter-rag-api/src/main/java/com/coverletter/coverletter_rag_api/experience/ctrl/ExperienceController.java
+++ backend/coverletter-rag-api/src/main/java/com/coverletter/coverletter_rag_api/experience/ctrl/ExperienceController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,6 @@ import com.coverletter.coverletter_rag_api.experience.domain.dto.ExperienceRespo
 import com.coverletter.coverletter_rag_api.experience.service.ExperienceService;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
