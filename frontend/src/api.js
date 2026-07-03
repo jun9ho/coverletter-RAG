91b5805c54ad93dc088a7a550384b3b4c9af630d
jun9ho <wjdgh9806@korea.ac.kr>
2026-07-03 21:44:19 +0900

    feat: FE 구현

---
+++ frontend/src/api.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const createExperience = async (data) => {
+  const response = await api.post("/api/experiences", data);
+  return response.data;
+};
+
+export const generateCoverLetter = async (data) => {
+  const response = await api.post("/api/cover-letters/generate", data);
+  return response.data;
+};
+
+export default api;
